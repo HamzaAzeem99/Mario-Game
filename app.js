1a37a2e630c70bf68bcdda59ed6ccd7e0c9f753e
HamzaAzeem99 <muhammadhamzamuhammadazeem@gmail.com>
2026-06-07 22:35:20 +0500

    fix

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-
 let mario_c = document.getElementById("mario_c")
 let enimy_c = document.getElementById("enimy_c")
 let jump_audio = new Audio("./audio/jump.mp3")
 let mario = new Audio("./audio/mario.mp3")
-// mario.play()
-// mario.volume = 0.3
-
 window.addEventListener("keydown", function (e) {
     if (e.key === " " || e.code === "Space") {
         mario_c.classList.add("mario_jump")
@@ -16,8 +12,6 @@ window.addEventListener("keydown", function (e) {
         console.log("jump");
 
     }
-
-
 })
 window.addEventListener("click", () => {
     mario_c.classList.add("mario_jump")
